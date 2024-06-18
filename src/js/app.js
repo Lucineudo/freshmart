e3fd8ded7826063cc3c7a51f922ccf390b066b63
lucineudo <lucineudodobaixo@hotmail.com>
2024-06-18 11:06:48 -0300

    Melhorias em todo o código deixando-o mais limpo mais adição do slick.

--- src/js/app.js
+++ src/js/app.js
@@ -14,9 +14,17 @@ $(document).ready(function () {
 	new Header()
 
 	$('.autoplay').slick({
-		slidesToShow: 3,
+		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: true,
 		autoplaySpeed: 2000,
+	})
+
+	$('.slider-produts').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: true,
 	})
 })
